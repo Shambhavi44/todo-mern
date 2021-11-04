ba84d71821677a87c0d8e3491c23df1e994df0b7
Shambhavi V <shambhavi13319@gmail.com>
2021-11-04 21:07:21 +0530

    Configure design structure

---
+++ root/client/src/components/CardDisplay.jsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { CardActions, Divider } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+
+export default function CardDisplay(props) {
+  const {
+    todoInfo = {},
+    onDeleteIconClick = () => {},
+    onEditIconClick = () => {},
+  } = props;
+  return (
+    <Card sx={{ maxWidth: 250 }} className="m-4">
+      <CardHeader
+        title={todoInfo.title}
+        subheader={todoInfo.date}
+        titleTypographyProps={{ variant: "h6" }}
+        subheaderTypographyProps={{ variant: "p", color: "white" }}
+        className="sticky-top bg-dark text-light"
+      />
+      <CardContent style={{ maxHeight: 200, overflowY: "auto" }}>
+        <Typography variant="body2" color="text.secondary">
+          {todoInfo.description}
+        </Typography>
+      </CardContent>
+      <Divider />
+      <CardActions className="d-flex justify-content-center">
+        <IconButton
+          aria-label="settings"
+          className="px-4"
+          onClick={onEditIconClick}
+        >
+          <EditIcon />
+        </IconButton>
+        <IconButton
+          aria-label="settings"
+          className="px-4"
+          onClick={onDeleteIconClick}
+        >
+          <DeleteIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+}
--- root/client/src/components/Home.jsx
+++ root/client/src/components/Home.jsx
@@ -1,9 +1,81 @@
-import React from "react";
-import Skeleton from "@mui/material/Skeleton";
-import { Button, Stack, TextField } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Button,
+  Grid,
+  // makeStyles,
+  Modal,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { Box } from "@mui/system";
+import CardDisplay from "./CardDisplay";
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: "flex",
+//     textAlign: "center",
+//     width: 300,
+//     marginLeft: 100,
+//     marginTop: 200,
+//   },
+//   hidden: {
+//     display: "none",
+//   },
+//   button: {
+//     background: "green",
+//   },
+//   label: {
+//     backgroundColor: "white",
+//   },
+// }));
 
 const Home = () => {
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "white !important",
+    boxShadow: 24,
+    p: 4,
+  };
+  // const classes = useStyles();
+
+  const [open, setOpen] = useState(false);
+  const dummyData = [
+    {
+      title: "Todo 1",
+      description: "This is todo 1 description",
+      id: "1",
+      date: "Sept 1 2021",
+    },
+    {
+      title: "Todo 4",
+      description: "This is todo 1 description",
+      id: "2",
+      date: "Sept 1 2021",
+    },
+    {
+      title: "Todo 3",
+      description: "This is todo 1 description",
+      id: "4",
+      date: "Sept 1 2021",
+    },
+    {
+      title: "Todo 7",
+      description: "This is todo 3 description",
+      id: "6",
+      date: "Sept 1 2021",
+    },
+    {
+      title: "Todo 7",
+      description: "This is todo 3 description",
+      id: "7",
+      date: "Sept 1 2021",
+    },
+  ];
+
   return (
     <div className="text-center my-5">
       <h2 className="my-5">Todo List Using MERN Stack</h2>
@@ -15,24 +87,82 @@ const Home = () => {
         noValidate
         autoComplete="off"
       >
-        <TextField
-          id="todoInput"
-          required
-          size="small"
-          fullWidth
-          placeholder="Enter a todo..."
-        />
-        <Button variant="contained" className="mt-2">
-          Add
+        <Button
+          variant="contained"
+          className="mt-2"
+          onClick={() => setOpen(true)}
+        >
+          Add a new Todo
         </Button>
       </Box>
-      <div>
-        {/* <Stack spacing={1}>
-          <Skeleton variant="text" />
-          <Skeleton variant="circular" width={40} height={40} />
-          <Skeleton variant="rectangular" width={210} height={118} />
-        </Stack> */}
-      </div>
+      <Grid
+        className="d-flex mx-2 body-bg rounded my-4"
+        style={{ maxHeight: "65vh", overflowY: "scroll" }}
+        container
+      >
+        {dummyData.map((item) => (
+          <Grid item lg={3}>
+            <CardDisplay className="col-lg-4" todoInfo={item} />
+          </Grid>
+        ))}
+      </Grid>
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box style={style} className="bg-white px-4 py-3 rounded">
+          <Typography
+            id="modal-modal-title"
+            variant="h5"
+            component="h2"
+            className="mt-2"
+          >
+            Add a new Todo
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 3 }}>
+            <TextField
+              id="todoInput"
+              required
+              size="small"
+              fullWidth
+              label="Todo Name"
+              variant="filled"
+            />
+
+            <TextField
+              id="todoInput"
+              required
+              size="small"
+              fullWidth
+              label="Todo Description"
+              multiline
+              rows={4}
+              className="my-4"
+              margin="normal"
+              variant="filled"
+              style={{ marginBottom: 20, marginTop: 20 }}
+            />
+          </Typography>
+          <Grid container justifyContent="flex-end" className="mt-2">
+            <Button
+              variant="contained"
+              className="mt-2 mx-3"
+              onClick={() => setOpen(true)}
+            >
+              Clear All
+            </Button>
+            <Button
+              variant="contained"
+              className="mt-2"
+              onClick={() => setOpen(true)}
+            >
+              Add
+            </Button>
+          </Grid>
+        </Box>
+      </Modal>
     </div>
   );
 };
